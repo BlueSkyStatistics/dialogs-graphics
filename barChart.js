61880666c0cc7f665a8586bf96d315141c667139
adpgithub <anildabralpro@gmail.com>
2024-11-14 11:37:56 +0530

    prepareExecution fix

--- barChart.js
+++ barChart.js
@@ -467,6 +467,7 @@ class barChartModal extends baseModal {
     }
     prepareExecution(instance) {
         var res = [];
+		let count = 0
         var errorBarOptions = "";
         if (instance.objects.y.el.getVal() == "") {
             var code_vars = {
@@ -568,7 +569,7 @@ class barChartModal extends baseModal {
             code_vars.selected.themes = themeRsyntax;
             let cmd = instance.dialog.renderR(code_vars)
             cmd = removenewline(cmd);
-            res.push({ cmd: cmd, cgid: newCommandGroup() })
+            res.push({ cmd: cmd, cgid: newCommandGroup(`${instance.config.id}`, `${instance.config.label}`), oriR: instance.config.RCode, code_vars: code_vars })
         }
         else {
             instance.objects.y.el.getVal().forEach(function (value) {
@@ -696,7 +697,13 @@ class barChartModal extends baseModal {
                 code_vars.selected.themes = themeRsyntax;
                 let cmd = instance.dialog.renderR(code_vars);
                 cmd = removenewline(cmd);
-                res.push({ cmd: cmd, cgid: newCommandGroup() })
+                if (count == 0) {
+                    res.push({ cmd: cmd, cgid: newCommandGroup(`${instance.config.id}`, `${instance.config.label}`), oriR: instance.config.RCode, code_vars: code_vars })
+                }
+                else {
+                    res.push({ cmd: cmd, cgid: newCommandGroup(), oriR: instance.config.RCode, code_vars: code_vars })
+                }
+                count++ 
             })
         }
         return res;
